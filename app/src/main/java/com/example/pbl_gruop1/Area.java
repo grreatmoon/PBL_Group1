d5fce730ec262d93096863380babaabf06ba71c7
grreatmoon <ayumoon08@gmail.com>
2025-10-12 15:45:56 +0900

    feat:Area及びAreaManagerクラスを追加

---
+++ app/src/main/java/com/example/pbl_gruop1/Area.java
@@ -0,0 +1,48 @@
+package com.example.pbl_gruop1;
+
+public class Area {
+
+    //エリアを識別するためのID
+    private String id;
+
+    //エリアの表示名
+    private String name;
+
+    //エリア中心の緯度
+    private double latitude;
+
+    //エリア中心の経度
+    private double longitude;
+
+    //エリアの半径(m)
+    private double radius;
+
+    //Areaオブジェクトを作成するためのコンストラクタ
+    public Area(String id, String name, double latitude, double longitude, double radius){
+        this.id = id;
+        this.name = name;
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+
+    //ゲッターセッター
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+}
